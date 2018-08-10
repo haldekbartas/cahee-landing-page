f794a9d9eeccdb9b955c2474d6dd6327f6f99adb
haldasbartlomiej <haldas.bartlomiej@gmail.com>
2018-08-10 17:33:21 +0200

    First commit

---
+++ myscripts.js
@@ -0,0 +1,18 @@
+//Smooth scrolling function
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+//Navbar background animation
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
